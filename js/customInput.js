e03c206e64b51adec38ed8f1049c6f1d98564b27
Muhammad Azizi <azizi@hotmail.com.my>
2024-06-23 09:49:00 +0800

    Complete the Connect section, pending buttons

--- js/customInput.js
+++ js/customInput.js
@@ -2,34 +2,50 @@ class customInput extends HTMLElement {
   constructor() {
     super();
 
-    let shadow = this.attachShadow({ mode: 'open' });
-    let template = document.getElementById('custom-input-template').content;
+    this.shadow = this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    // if (!this.inputEl || !this.placeholderEl) {
+    //   console.error('Failed to find input or placeholder');
+    //   if (this.placeholderEl) {
+    //     this.placeholderEl.innerHTML = 'Failed to load';
+    //   }
+    //   if (this.inputEl) {
+    //     this.inputEl.setAttribute('disabled', 'true');
+    //   }
+    //   return;
+    // }
+
+    let multiline = this.getAttribute('multiline');
+    this.isMultiLine = multiline == 'true';
+    
+    let template;
+    if (this.isMultiLine) {
+      template = document.getElementById('custom-textarea-template').content;
+    } else {
+      template = document.getElementById('custom-input-template').content;
+    }
+
+    console.log(this.isMultiLine);
+    console.log(template);
+
     let templateClone = template.cloneNode(true);
 
     this.inputEl = templateClone.getElementById('custom-input-element');
     this.placeholderEl = templateClone.getElementById('custom-placeholder-element');
 
-    shadow.appendChild(templateClone);
-  }
+    this.shadow.appendChild(templateClone);
 
-  connectedCallback() {
-    if (!this.inputEl || !this.placeholderEl) {
-      console.error('Failed to find input or placeholder');
-      if (this.placeholderEl) {
-        this.placeholderEl.innerHTML = 'Failed to load';
-      }
-      if (this.inputEl) {
-        this.inputEl.setAttribute('disabled', 'true');
-      }
-      return;
-    }
-
+    //#region Attributes
     let placeholder = this.getAttribute('placeholder');
     this.placeholderEl.innerHTML = placeholder || 'Placeholder not set';
 
     let elKey = this.getAttribute('key');
     this.elementKey = elKey || '';
+    //#endregion Attributes
 
+    //#region Events
     this.inputEl.addEventListener('focus', (e) => {
       if (!Array.from(this.placeholderEl.classList).includes('custom-placeholder-focus')) {
         this.placeholderEl.classList.add('custom-placeholder-focus');
@@ -52,6 +68,8 @@ class customInput extends HTMLElement {
 
       this.dispatchEvent(event);
     });
+    //#endregion Events
+
   }
 }
 
