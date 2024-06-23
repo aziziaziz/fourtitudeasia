9d502834d6ec8b9f3c9dfd70970ea170efa5320b
Muhammad Azizi <azizi@hotmail.com.my>
2024-06-23 17:41:35 +0800

    Added comments

--- js/customInput.js
+++ js/customInput.js
@@ -2,6 +2,7 @@ class customInput extends HTMLElement {
   constructor() {
     super();
 
+    // Creating the shadow of the current document
     this.shadow = this.attachShadow({ mode: 'open' });
   }
 
@@ -10,41 +11,51 @@ class customInput extends HTMLElement {
     this.isMultiLine = multiline == 'true';
     
     let template;
+    // Get the template whether it is multiline (textarea) or not (input)
     if (this.isMultiLine) {
       template = document.getElementById('custom-textarea-template').content;
     } else {
       template = document.getElementById('custom-input-template').content;
     }
 
+    // To clone the template and work on the clone instead
     let templateClone = template.cloneNode(true);
 
+    // Get the input/textarea element and the placeholder
     this.inputEl = templateClone.getElementById('custom-input-element');
     this.placeholderEl = templateClone.getElementById('custom-placeholder-element');
-
+    
+    // Appending the cloned template
     this.shadow.appendChild(templateClone);
 
     //#region Attributes
+    // Set the placeholder
     let placeholder = this.getAttribute('placeholder');
     this.placeholderEl.innerHTML = placeholder || 'Placeholder not set';
 
+    // Set the key for easier handling when the input value changed
     let elKey = this.getAttribute('key');
     this.elementKey = elKey || '';
 
+    // Setting the type of the input
     let elType = this.getAttribute('type');
     this.inputEl.setAttribute('type', elType || 'text');
     //#endregion Attributes
 
     //#region Events
+    // When focus on the input, the placeholder will be moved up
     this.inputEl.addEventListener('focus', (e) => {
       if (!Array.from(this.placeholderEl.classList).includes('custom-placeholder-focus')) {
         this.placeholderEl.classList.add('custom-placeholder-focus');
       }
     });
+    // When unfocus, the placeholder will come back to original place if the value is empty
     this.inputEl.addEventListener('blur', (e) => {
       if (!e.target.value) {
         this.placeholderEl.classList.remove('custom-placeholder-focus');
       }
     });
+    // When the input value changed
     this.inputEl.addEventListener('input', (e) => {
       let value = e.target.value;
 
@@ -55,6 +66,7 @@ class customInput extends HTMLElement {
 
       this.inputEl.value = value;
 
+      // Create and dispatch the custom input changed event to be consumed by the parent of this custom element
       let event = new CustomEvent('customInputChanged', {
         detail: {
           value: value,
@@ -70,6 +82,7 @@ class customInput extends HTMLElement {
   }
 
   //#region Methods
+  // Function to clear the input value from parent side
   clear() {
     this.inputEl.value = '';
     if (Array.from(this.placeholderEl.classList).includes('custom-placeholder-focus')) {
@@ -79,4 +92,5 @@ class customInput extends HTMLElement {
   //#endregion Methods
 }
 
+// Defining a custom element to be used in the HTML
 customElements.define('custom-input', customInput);
--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ let mobileMenuShow = false;
 document.addEventListener('DOMContentLoaded', () => {
   connectButtonEnabled(false);
 
+  // Event handler when the value of the custom input changed
   document.body.addEventListener('customInputChanged', (e) => {
     let allKeys = Object.keys(connectObj);
     if (allKeys.includes(e.detail.key)) {
@@ -24,7 +25,9 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+// When clearing the connect details
 function clearClicked() {
+  // Getting the custom input and clear via the clear method
   let name = document.getElementById('connect-name');
   let address = document.getElementById('connect-address');
   let contact = document.getElementById('connect-contact');
@@ -34,6 +37,7 @@ function clearClicked() {
   contact.clear();
   details.clear();
 
+  // Need to remove all the values in the object as well
   connectObj = {
     name: '',
     address: '',
@@ -41,16 +45,18 @@ function clearClicked() {
     details: ''
   };
 
+  // Disabling the connect button
   connectButtonEnabled(false);
 }
 
+// When connect button is clicked
 function connectClicked() {
   let verifyEmail = connectObj.address.match(/^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/g);
   let verifyContact = connectObj.contact.match(/^\+?[0-9()-]{7,20}$/g)
-  if (verifyEmail && verifyContact) {
+  if (verifyEmail && verifyContact) { // If the email address and the phone number is verified and in correct format
     alert('Thank you! We will get in touch with you as soon as possible.');
     clearClicked();
-  } else {
+  } else { // Either the email or phone number is not in correct format
     if (!verifyEmail && !verifyContact) {
       alert('Please provide a valid email address and phone number.')
     } else if (!verifyEmail) {
@@ -61,6 +67,7 @@ function connectClicked() {
   }
 }
 
+// To enable to disable the connect button
 function connectButtonEnabled(enable) {
   let connectButton = document.getElementById('button-connect');
   if (enable) {
@@ -70,24 +77,25 @@ function connectButtonEnabled(enable) {
   }
 }
 
+// To show or hide the mobile menu using the burger icon
 function showHideMobileMenuClicked(close) {
   let mobileBurger = document.getElementById('mobile-burger');
   let mobileBurgerClose = document.getElementById('mobile-burger-close');
   let navigator = document.getElementById('mobile-navigator');
 
-  if (close) {
+  if (close) { // Close is provided means that the menu is forced to close
     mobileMenuShow = false;
-  } else {
+  } else { // Based on the current state of the menu, if close, then open and vice versa
     mobileMenuShow = !mobileMenuShow;
   }
 
-  if (mobileMenuShow) {
+  if (mobileMenuShow) { // Hide the burger icon, show the close icon and show the menu
     mobileBurger.classList.add('mobile-menu-hide');
     mobileBurgerClose.classList.remove('mobile-menu-hide');
 
     navigator.classList.add('mobile-navigator-show');
     navigator.classList.remove('mobile-navigator-hide');
-  } else {
+  } else { // Show the burger icon, hide the close icon and hide the menu
     mobileBurger.classList.remove('mobile-menu-hide');
     mobileBurgerClose.classList.add('mobile-menu-hide');
 
@@ -96,12 +104,14 @@ function showHideMobileMenuClicked(close) {
   }
 }
 
+// When the menu is clicked, scroll into the view
 function scrollToView(id, isMobile) {
   let el = document.getElementById(id);
   window.scrollTo({
-    top: el.offsetTop - (isMobile ? 45 : 60),
+    top: el.offsetTop - (isMobile ? 45 : 60), // The element top offset had to be minused with a fix value based on the UI whether it is mobile or not
     behavior: 'smooth'
   });
 
+  // To force the menu to be closed/hide
   showHideMobileMenuClicked(true);
 }
